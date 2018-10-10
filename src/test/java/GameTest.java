c84c35091984bd7bd3c8b2e0ef1efd215a90a6f9
gladysleng <gladys_yinyuen@hotmail.com>
2018-10-09 21:19:45 -0400

    done but maven

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -396,4 +396,8 @@ public class GameTest {
 
         assertEquals(expectedWinner,actualWinner);
     }
+
+
+    //@org.junit.Rule
+    //public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
 }
